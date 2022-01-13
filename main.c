1288c9a604c305467ea8bd85dc7faaf4c5782ece
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-01-13 04:07:21 +0000

    Monty 12/01/2022 11:00PM

--- main.c
+++ main.c
@@ -38,6 +38,27 @@ int count_words(char *s)
 }
 
 /**
+ * count_lines - Count lines of a string
+ * @s: String to count lines
+ * Return: Number of lines
+ */
+
+int count_lines(char *s)
+{
+	int ret, i;
+
+	ret = 0;
+	if (s == NULL)
+		return (ret);
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == '\n')
+			ret++;
+	}
+	return (ret);
+}
+
+/**
  * linetoarray - transforms line into an array
  * @line: Line to transform
  * Return: Built array, or NULL in failure
@@ -51,17 +72,18 @@ char **linetoarray(char *line)
 	words = count_words(line);
 	if (words == 0)
 		return (NULL);
-	ret = malloc(sizeof(*ret) * words);
+	ret = malloc((sizeof(*ret) * words) + 1);
 	if (!ret)
 		return (NULL);
 	replacetabs(line);
 	act_word = strtok(line, " ");
-	while(act_word)
+	while (act_word)
 	{
 		ret[i] = act_word;
 		i++;
 		act_word = strtok(NULL, " ");
 	}
+	ret[i] = NULL;
 	return (ret);
 }
 
@@ -74,36 +96,51 @@ char **linetoarray(char *line)
 
 int main(int argc, char **argv)
 {
-	char *buff, *line;
-	int fd, len_read = 1;
+	char *buff, *line, **exec_array, **copied_lines;
+	int fd, lines, len_read = 1, i = 0;
+	void (*handling_function)(stack_t **, unsigned int);
 
 	if (argc != 2)
 	{
-		perror("USAGE: monty file\n");
+		write(2, "USAGE: monty file\n", _strlen("USAGE: monty file\n"));
 		exit(EXIT_FAILURE);
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		perror("Error: Can't open file");
-		perror(argv[1]);
-		perror("\n");
+		write(2, "Error: Can't open file ", _strlen("Error: Can't open file "));
+		write(2, argv[1], _strlen(argv[1]));
+		write(2, "\n", 1);
 		exit(EXIT_FAILURE);
 	}
 	buff = _calloc(1024, sizeof(*buff));
 	if (buff == NULL)
 	{
-		perror("Error: malloc failed\n");
+		write(2, "Error: malloc failed\n", _strlen("Error: malloc failed\n"));
 		exit(EXIT_FAILURE);
 	}
 	while (len_read > 0)
 		len_read = read(fd, buff, 1024);
+	lines = count_lines(buff);
+	copied_lines = malloc(sizeof(*copied_lines) * (lines + 2));
+	if (!copied_lines)
+		exit(EXIT_FAILURE);
 	line = strtok(buff, "\n");
 	while (line)
 	{
-		printf("%s\n", line);
-		line = strtok(NULL, "\n");
+		copied_lines[i] = line;
+	    i++;
+	    line = strtok(NULL, "\n");
 	}
+	copied_lines[i] = NULL;
+	for(i = 0; copied_lines[i] != NULL; i++)
+	{
+		exec_array = linetoarray(copied_lines[i]);
+		handling_function = getopcode_fun(exec_array[0]);
+		if (!handling_function)
+			exit(EXIT_FAILURE);
+	}
+	free(copied_lines);
 	free(buff);
 	close(fd);
 	return (0);
--- monty.h
+++ monty.h
@@ -28,7 +28,7 @@ typedef struct stack_s
  * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
-{
+{	
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
@@ -47,5 +47,17 @@ typedef struct instruction_s
 /** functions **/
 
 void *_calloc(unsigned int nmemb, unsigned int size);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+void (*getopcode_fun(char *opcode))(stack_t **, unsigned int);
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
 
 #endif
