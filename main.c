6da3ebc71e2b25b2fa80b252d342cdf2d11bf158
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-01-13 21:38:58 +0000

    Monty 13/01/2022 4:40PM

--- main.c
+++ main.c
@@ -96,6 +96,7 @@ int main(int argc, char **argv)
 	if (fd == -1)
 		OPEN_ERROR(argv[1]);
 	read(fd, buff, 1024);
+	close(fd);
 	replace_emptylines(buff, copied_lines);
 	line = strtok(buff, "\n");
 	lines_to_array(line, copied_lines);
@@ -111,6 +112,5 @@ int main(int argc, char **argv)
 	}
 	if (stack)
 		free_dlistint(stack);
-	close(fd);
 	return (0);
 }
